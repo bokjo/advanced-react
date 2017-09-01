8071b1cdd8125b810c98e94ef26719104c2af843
bokjo <bstojcevski89@gmail.com>
2017-09-01 15:56:34 +0200

    Added styling to the Article component

--- src/components/Article.js
+++ src/components/Article.js
@@ -1,20 +1,55 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+const styles = {
+
+  article: {
+    paddingBottom: 15,
+    borderBottomStyle: 'solid',
+    borderBottomColor: '#eee',
+    borderBotomWidth: .71,
+    marginBottom: 10,
+  },
+  title: {
+    fontWeight: 'bold',
+  },
+  date: {
+    fontSize: '0.65em',
+    color: '#888',
+    fontFamily: 'Monospace',
+  } ,
+  author: {
+    paddingTop: 10,
+    paddingBottom: 10,
+    fontFamily: 'Inconsolata-dz',
+  },
+  link: {
+    color: '#32FA9D'
+  },
+  body: {
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+};
+
+const formatDate = (dateToFormat) =>
+  new Date(dateToFormat).toDateString();
+
 const Article = (props) => {
   const { article, author } = props;
-  //console.log("HERE");
-  //console.log(props);
+
   return (
-    <div>
-      <div>{article.title}</div>
-      <div>{article.date}</div>
-      <div>
-        <a href={author.website}>
+    <div style={styles.article}>
+      <div style={styles.title}>{article.title}</div>
+      <div style={styles.date}>
+        {formatDate(article.date)}
+      </div>
+      <div style={styles.author}>
+        <a style={styles.link} href={author.website}>
           {author.firstName} {author.lastName}
         </a>
       </div>
-      <div>{article.body}</div>
+      <div style={styles.body}>{article.body}</div>
     </div>
   );
 };
